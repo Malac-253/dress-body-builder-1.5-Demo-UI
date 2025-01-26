9b96dc37080aee686bf18ed68c9c757db2e435c6
malac-253 <malachibeerram1@gmail.com>
2025-01-25 19:57:25 -0500

    new code

--- configs/configs.js
+++ configs/configs.js
@@ -1,9 +1,8 @@
 const API_BASE = "https://dress-body-builder-2.onrender.com/api/";
-const GUEST_USER_TOKEN = "e41cac5b45048591b81cf84a7070e78765a58be6"; // Public UI Only
-const ACCESS_CODE = "6tosDJj29gvSJ7xdu3LsO0LoVgZOoeB8"; // Public UI Only
+const GUEST_USER_TOKEN = "e41cac5b45048591b81cf84a7070e78765a58be6"; // Public Guest Only
+const ACCESS_CODE = "gGLmcMl6x8TRlYnJXyBCSNPEjttcX8qn"; // Public facing UI Only
 
 // Configs
 
 const TARGET = "_blank" // (tab target) Always open new tab ["_blank","_self"]
-
 const SKIP_TYPES = ["eyes","mask","neck"]; // all lowercase
