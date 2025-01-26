89149b8e2624977e2cd8259e241471790ec868dc
malac-253 <malachibeerram1@gmail.com>
2025-01-25 20:32:58 -0500

    config fix

--- configs/configs.js
+++ configs/configs.js
@@ -6,3 +6,23 @@ const ACCESS_CODE = "gGLmcMl6x8TRlYnJXyBCSNPEjttcX8qn"; // Public facing UI Only
 
 const TARGET = "_blank" // (tab target) Always open new tab ["_blank","_self"]
 const SKIP_TYPES = ["eyes","mask","neck"]; // all lowercase
+
+const ANIM_STYLE_ID = 3;
+
+const ROWS_PER_FETCH = 5;
+
+const TYPE_ORDER = {
+  hairaccessory:1,
+  mouth: 2,
+  eyebrows: 3,
+  hair: 4,
+  shirt: 5,
+  sleeves: 6,
+  pants: 7,
+  shoes: 8,
+  tail:9,
+  wingl:10,
+  wingr:11,
+  rightspears:12,
+  leftyspears:13
+};
